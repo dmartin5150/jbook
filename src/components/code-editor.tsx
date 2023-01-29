318b33e6d073a91720facf9018d3450356b508c8
David Martin <dmartin5150@icloud.com>
2023-01-29 17:55:38 -0600

    added code highlighting

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,9 +1,13 @@
 import './code-editor.css';
+import './syntax.css';
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import { useRef } from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
+import codeShift  from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
+
 
 interface CodeEditorProps {
   initialValue: string;
@@ -22,6 +26,21 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
     });
 
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+
+    const highlighter = new Highlighter(
+      //@ts-ignore
+      window.monaco,
+      codeShift,
+      monacoEditor);
+
+    highlighter.highLightOnDidChangeModelContent(
+      ()=> {},
+      ()=> {},
+      undefined,
+      ()=>{}
+    );
+
+
   };
 
 
