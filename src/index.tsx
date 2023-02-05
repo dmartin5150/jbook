92b490c4177b9eaa12be9d3dd4ad56167d274d4e
David Martin <dmartin5150@icloud.com>
2023-02-05 12:09:18 -0600

    added action bar

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import ReactDOM from 'react-dom/client';
 import TextEditor from './components/text-editor';
 import { Provider } from 'react-redux';
 import { store } from './state';
+import CellList from './components/cell-list';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
