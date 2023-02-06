f78854cbfac58f132176dc401528e95525f856fa
David Martin <dmartin5150@icloud.com>
2023-02-05 22:10:53 -0600

    added styling to action bar

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,18 +1,26 @@
-import { useTypedSelector } from "../hooks/use-typed-selector";
-import CellListItem from "./cell-list-item";
+import { useTypedSelector } from '../hooks/use-typed-selector';
+import { Fragment } from 'react';
+import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
 
-
-const CellList: React.FC =() =>{
-  const cells = useTypedSelector(({cells:{order,data}}) => {
-    return order.map((id)=>{
+const CellList: React.FC = () => {
+  const cells = useTypedSelector(({ cells: { order, data } }) => {
+    return order.map((id) => {
       return data[id];
-    })
+    });
   });
 
+  const renderedCells = cells.map((cell) => (
+    <Fragment key={cell.id}>
+      <AddCell  nextCellId={cell.id}/>
+      <CellListItem cell={cell} />
+    </Fragment>
+  ));
 
-  const renderedCells = cells.map((cell)=> <CellListItem key={cell.id} cell={cell}/>);
-
-  return <div>{renderedCells}</div>
-}
+  return <div>
+    {renderedCells}
+    <AddCell forceVisible={cells.length === 0} nextCellId={null}/>
+  </div>;
+};
 
 export default CellList;
